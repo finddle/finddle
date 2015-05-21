16bd63dcc30739c50c42d782ad06a8d08878cca4
andresaj <andresaguirrej@hotmail.com>
2015-05-21 13:43:30 +0200

    no message

--- includes/php/comentariosBD.php
+++ includes/php/comentariosBD.php
@@ -1,4 +1,48 @@
 <?php
 require_once(__DIR__."/config.php");
 
+function getComentarios($var){
+global $mysqli;
+$query= "SELECT nickUsuario, Texto FROM Comentarios WHERE IDEvento='$var';";
+$resultado=$mysqli->query($query)
+	or die ($mysqli->error. " en la línea ".(__LINE__-1));
+
+	while ($registro = $resultado->fetch_assoc()) {
+		echo '<div id="comentario">';
+				
+				echo '<div id="nick">',$registro["nickUsuario"],"   ", "</div>";
+				
+			
+				echo '<div id="texto">',$registro["Texto"], "</div>";
+				
+		echo "</div>";
+		
+	}
+	
+$resultado->free();
+}
+
+function commentEvent($user, $event, $comment){
+
+global $mysqli;
+$query= "INSERT into Comentarios (NULL, '$user', '$event', '$comment');";
+$resultado=$mysqli->query($query)
+	or die ($mysqli->error. " en la línea ".(__LINE__-1));
+$resultado->free();
+
+}
+
+function eliminarComentario($comment){
+
+global $mysqli;
+$query= "DELETE from Comentarios WHERE '$comment"=ID;
+$resultado=$mysqli->query($query)
+	or die ($mysqli->error. " en la línea ".(__LINE__-1));
+$resultado->free();
+
+
+}
+
+
+
 ?>
--- includes/php/eventosBD.php
+++ includes/php/eventosBD.php
@@ -1,4 +1,25 @@
 <?php
 require_once(__DIR__."/config.php");
 
+function addPeticionEvento($nombre, $descripcion, $fecha, $precio, $imagen, $plazasDisponibles, $tipo, $promotor){
+
+global $mysqli;
+mysqli_real_escape_string($mysqli, $nombre);
+mysqli_real_escape_string($mysqli, $descripcion);
+mysqli_real_escape_string($mysqli, $fecha);
+mysqli_real_escape_string($mysqli, $precio);
+mysqli_real_escape_string($mysqli, $imagen);
+mysqli_real_escape_string($mysqli, $plazasDisponibles);
+mysqli_real_escape_string($mysqli, $tipo);
+mysqli_real_escape_string($mysqli, $promotor);
+
+$query= "INSERT into eventos (NULL, '$nombre', '$descripcion', '$fecha', '$precio', '$imagen', '$plazasDisponibles', '$tipo', '$promotor', 0);";
+$resultado=$mysqli->query($query)
+	or die ($mysqli->error. " en la línea ".(__LINE__-1));
+$resultado->free();
+
+}
+
+
+
 ?>
