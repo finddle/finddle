d4de75e29805d06e822146ed942339d3b164a843
andresaj <andresaguirrej@hotmail.com>
2015-06-04 12:50:16 +0200

    Ya se muestran los comentarios y se permite añadir comentarios.

--- includes/php/comentariosBD.php
+++ includes/php/comentariosBD.php
@@ -2,47 +2,68 @@
 require_once(__DIR__."/config.php");
 
 function getComentarios($var){
-global $mysqli;
-$query= "SELECT nickUsuario, Texto FROM Comentarios WHERE IDEvento='$var';";
-$resultado=$mysqli->query($query)
-	or die ($mysqli->error. " en la línea ".(__LINE__-1));
+	global $mysqli;
 
-	while ($registro = $resultado->fetch_assoc()) {
-		echo '<div id="comentario">';
-				
-				echo '<div id="nick">',$registro["nickUsuario"],"   ", "</div>";
-				
-			
-				echo '<div id="texto">',$registro["Texto"], "</div>";
-				
-		echo "</div>";
-		
+	$args = array($var);
+	sanitizeArgs($args);
+
+	$pst = $mysqli->prepare("SELECT * FROM comentarios WHERE IDEvento = ? ORDER BY Fecha DESC;");
+	$pst->bind_param("i", $args[0]);
+
+		$pst->execute();
+	$result = $pst->get_result();
+	$comentario = null;
+
+	while($row = $result->fetch_array(MYSQLI_ASSOC)){
+		$comentario[] = $row;
 	}
-	
-$resultado->free();
+	$pst->close();
+	return $comentario;
+
+	/*
+		while ($registro = $resultado->fetch_assoc()) {
+			echo '<div id="comentario">';
+					
+					echo '<div id="nick">',$registro["nickUsuario"],"   ", "</div>";
+					
+				
+					echo '<div id="texto">',$registro["Texto"], "</div>";
+					
+			echo "</div>";
+			
+		}
+	*/
+
 }
 
 function commentEvent($user, $event, $comment){
+// funcion now();
+	global $mysqli;
+	$args = array($user,$event,$comment);
+	sanitizeArgs($args);
+	$fecha=strftime("%Y-%m-%d-%H-%M:%S", time());
 
-global $mysqli;
-$query= "INSERT into Comentarios (NULL, '$user', '$event', '$comment');";
-$resultado=$mysqli->query($query)
-	or die ($mysqli->error. " en la línea ".(__LINE__-1));
-$resultado->free();
+	$pst = $mysqli->prepare("INSERT into comentarios VALUES ('', ?, ?, ?, ?);");
+	$pst->bind_param("siss",$args[0],$args[1],$args[2],$fecha);
+	$pst->execute();
+	print_r($args);
+	print_r($fecha);
+	$pst->close();
 
 }
 
 function eliminarComentario($comment){
 
-global $mysqli;
-$query= "DELETE from Comentarios WHERE '$comment"=ID;
-$resultado=$mysqli->query($query)
-	or die ($mysqli->error. " en la línea ".(__LINE__-1));
-$resultado->free();
+	global $mysqli;
+	$args = array($comment);
+	sanitizeArgs($args);
 
+	$pst = $mysqli->prepare("DELETE from comentarios WHERE ?=ID;");
+	$pst->bind_param("i",$args[0]);
+	$pst->execute();
+	$pst->close();
 
 }
-
 
 
 ?>
--- infoEvento.php
+++ infoEvento.php
@@ -11,15 +11,27 @@
 	  <link rel="stylesheet" type="text/css" href="includes/css/mycss.css">
 	  <!--Favicon-->
 	  <link rel="shortcut icon" href="includes/img/favicon.png" />
+
 	</head>
+	
+	<?php 
+		
+		require(__DIR__.'/includes/php/header.php');  
+		require(__DIR__.'/includes/php/eventosBD.php');
+		require(__DIR__.'/includes/php/comentariosBD.php');
+	
+		if(isset($_POST['comentario'])) {
+        	//$result = comentario($_POST);
+        	print_r($_POST);
+        	$idEvent=$_POST['idEvento'];
+        	$usr=$_SESSION['username'];
+        	$com=$_POST['comentario'];
+        	commentEvent($usr, $idEvent, $com);
+    }
+	?>
+	
 	<body>
 	
-		<?php 
-			require(__DIR__.'/includes/php/header.php');  
-			require_once(__DIR__.'/includes/php/eventosBD.php');
-			require_once(__DIR__.'/includes/php/asisteBD.php');
-		?>
-
 	  <!--Inicio Contenido-->
 	  <div class="main">
 		<div class="container">
@@ -33,38 +45,51 @@
 			 <?php 
 				$evento = $_GET['evento'];
 				$info = getInfoEvento($evento);
-				$nAsistentes = countAsistentes($info['ID']);
 				echo '<div class="eventosElem">';
 				echo '<h2>'.$info['Nombre'].'</h2>';
 				echo '<p>Fecha: '.$info['Fecha'].'</p>';
 				echo '<p>Descripcion: '.$info['Descripcion'].'</p>';
 				echo '<p>Plazas: '.$info['PlazasDisponibles'].'</p>';
-				echo '<p>Asistentes: '.$nAsistentes.'</p>';
 				echo '<p><img src ="'.$info['Imagen'].'"/></p>';
 				if(isset($_SESSION['username'])){
-					if($nAsistentes < $info['PlazasDisponibles']){
-						if($info['Tipo']==0){
-							echo '<p><a class="btn btn-primary" href="comprarEntradaFiesta.php?evento='.$info['ID'].'">Comprar Entrada</a></p>';	
-						}else {
-							echo '<p><a class="btn btn-primary" href="comprarEntradaCine.php?evento='.$info['ID'].'">Comprar Entrada</a></p>';
-						}
-						
-					}else{
-						echo '<p>Lo sentimos, las entradas se han agotado.</p>';
-					}
+					echo '<p><a href="comprarEntrada.php?evento='.$info['ID'].'&tipo='.$info['Tipo'].'">Comprar Entrada</a></p>';
+					?>
+					<textarea name="comentario" cols="74" rows="4" autofocus form="usrform"> Escribe aquí tu comentario! </textarea>
+					<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" id="usrform">
+  						<input type="hidden" name="idEvento" value="<?php echo $evento;?>">
+  						<input type="submit"> 
+					</form>
+					<?php
 				}else{
-					echo '<div class="alert alert-info" role="alert">
-					<span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span>
- 					Inicia sesion para comprar tu entrada a este evento</div> ';
+					echo '<p>!Inicia sesion para comprar tu entrada a este evento!</p>';
+				}
+				
+				//commentEvent($user, $event, $comment);
+				$coment = getComentarios($evento);
+				foreach ($coment as $comentario) {
+					echo '<div id="comentario">';
+					
+					echo '<div id="nick">',$comentario["NickUsuario"],"   ", "</div>";
+					echo '<div id="texto">',$comentario["Texto"], "</div>";
+					
+					echo "</div>";
 				}
 				echo '</div>';
 			 ?>
+
+
+
 		  </div>
 
 		  
 		  <div class="clearfix visible-xs-block visible-sm-block"></div>
 		  <div class="sidebar-right container-fixed col-xs-4 col-sm-4 col-md-3"></div>
 		</div>
+
+
+
+
+
 	  </div>
 	  
 	  <!--Fin Contenido-->
