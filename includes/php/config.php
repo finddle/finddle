9c2265652c4c7aee8a421c8198ebf69f2bbb6e28
mizadri <adrigarvk8@gmail.com>
2015-06-25 12:28:31 +0200

    /*Funcion para esquivar el error por la falta del driver mysqlnd en el hosting web que saca una excepcion al hacer $pst->get_result()*/

--- includes/php/config.php
+++ includes/php/config.php
@@ -34,4 +34,23 @@ function cierraConexion() {
     $mysqli->close();
   }
 }
+
+/*Funcion para esquivar el error por la falta del driver mysqlnd 
+en el hosting web que saca una excepcion al hacer $pst->get_result()*/
+function fetchAssocStatement($stmt){
+    if($stmt->num_rows>0)
+    {
+        $result = array();
+        $md = $stmt->result_metadata();
+        $params = array();
+        while($field = $md->fetch_field()) {
+            $params[] = &$result[$field->name];
+        }
+        call_user_func_array(array($stmt, 'bind_result'), $params);
+        if($stmt->fetch())
+            return $result;
+    }
+
+    return null;
+}
 ?>
